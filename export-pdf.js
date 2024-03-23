640d9203e733a712731bd6e9db13d38917af4e5c
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2024-03-23 19:19:44 +0200

    fix building pdf

--- export-pdf.js
+++ export-pdf.js
@@ -1,27 +1,19 @@
-import { promises as fs } from 'fs';
-import * as theme from 'jsonresume-theme-eshaham';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import puppeteer from 'puppeteer';
-import { render } from 'resumed';
 
 const args = yargs(hideBin(process.argv)).parse();
-const resumeFilename = args._[0];
-const resumePdfFilename = args._[1];
+const resumePdfFilename = args._[0];
 
-if (!resumeFilename || !resumePdfFilename) {
-  console.error('Usage: node export-pdf.js <resume.json> <resume.pdf>');
+if (!resumePdfFilename) {
+  console.error('Usage: node export-pdf.js <resume.pdf>');
   process.exit(1);
 }
-
-const resumeFileStr = await fs.readFile(resumeFilename, 'utf-8');
-const resume = JSON.parse(resumeFileStr);
-const html = await render(resume, theme);
 
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
-await page.setContent(html, { waitUntil: 'networkidle0' });
+await page.goto('http://localhost:8080', { waitUntil: 'networkidle0' });
 await page.pdf({
   path: resumePdfFilename,
   format: 'a4',
